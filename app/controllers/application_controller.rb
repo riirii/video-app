c8a87d7ad6260258c174fbb67556cc315da40181
riirii <ryochan5630@yahoo.co.jp>
2020-10-08 18:01:06 +0900

    新規登録・ログイン画面実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
- 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
 
 
@@ -12,7 +13,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 
 
 end
