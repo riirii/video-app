b8a100eb165279b222f73fe2af35a087d66d0ed3
riirii <ryochan5630@yahoo.co.jp>
2020-10-11 00:50:49 +0900

    機能選択画面の基礎実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'groups/index'
+  root to: "groups#select"
   devise_for :users
+
+  resources :groups, only: [:index, :show, :new, :create, :destroy, :edit] do
+    collection do 
+     get 'select'
+    end
+ end
 end
