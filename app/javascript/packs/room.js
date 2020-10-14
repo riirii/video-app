72a16ed300061040fc18eb2c7bd283d15467de97
riirii <ryochan5630@yahoo.co.jp>
2020-10-14 17:48:13 +0900

    HTML修正

--- app/javascript/packs/room.js
+++ app/javascript/packs/room.js
@@ -1,6 +1,6 @@
 import Vue from 'vue/dist/vue.esm'
 import Room from '../room.vue'
-
+// room.vueファイルからRoomモジュールをインポートしてる
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#room',
