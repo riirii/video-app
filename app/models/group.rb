2595650dfe89c154c8175e4cd49231b9d55023d8
riirii <ryochan5630@yahoo.co.jp>
2020-10-18 01:41:50 +0900

    プルダウン実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,9 @@
 class Group < ApplicationRecord
 
-has_many :group_users
-has_many :users, through: group_users
-has_many :messages
+  has_many :group_users
+  has_many :users, through: :group_users
+  has_many :messages
 
-validates :name, presence: true
+  validates :name, presence: true
 
 end
