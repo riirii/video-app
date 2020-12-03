0356f96b5b4db810449aa0536953848960abb002
riirii <ryochan5630@yahoo.co.jp>
2020-12-03 16:29:43 +0900

    ビデオ通話のサンプルコード入力

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 // require("channels")
 require('jquery');
-// require("../checked")
+require("../script")
 // require("hello_vue")
 // require("room.js")
 // require("../app")
